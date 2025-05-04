963a5d60493a93704f150c73572a2a7c802a275d
apianta <a.pianta92@gmail.com>
2025-05-04 12:30:38 -0400

    feat: initialize typing game project with HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,77 @@
+const quotes = [
+  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
+  "There is nothing more deceptive than an obvious fact.",
+  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
+  "I never make exceptions. An exception disproves the rule.",
+  "What one man can invent another can discover.",
+  "Nothing clears up a case so much as stating it to another person.",
+  "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
+]
+// store the list of words and the index of the word the player is currently typing
+let words = []
+let wordIndex = 0
+// starting time
+let startTime = Date.now()
+// page elements
+const quoteElement = document.getElementById("quote")
+const messageElement = document.getElementById("message")
+const typedValueElement = document.getElementById("typed-value")
+
+// Start and track
+document.getElementById("start").addEventListener("click", () => {
+  // get a quote
+  const quoteIndex = Math.floor(Math.random() * quotes.length)
+  const quote = quotes[quoteIndex]
+  //   put the quote into an array of words
+  words = quote.split(" ")
+  //   reset the word index for tracking
+  wordIndex = 0
+
+  //  UI updates
+  // Create an array of span elements so we can set a class
+  const spanWords = words.map(function (word) {
+    return `<span>${word}</span>`
+  })
+  //   Convert into string and set as innerHTML on quote display
+  quoteElement.innerHTML = spanWords.join("")
+  //   Highlight the first word
+  quoteElement.childNodes[0].className = "highlight"
+  // Clear any prior messages
+  messageElement.innerText = ""
+
+  //   Setup the textbox
+  //   Clear the textbox
+  typedValueElement.value = ""
+  //   Set focus
+  typedValueElement.focus()
+  //   Set the event handler
+
+  //   Start the timer
+  startTime = new Date().getTime()
+})
+
+typedValueElement.addEventListener("input", () => {
+  // Get the current word
+  const currentWord = words[wordIndex]
+  // Get the current value
+  const typedValue = typedValueElement.value
+
+  if (typedValue === currentWord && wordIndex === words.length - 1) {
+    // end of sentence
+    // display success
+    const elapsedTime = new Date().getTime() - startTime
+    const message = `CONGRATULATIONS! You finished in ${
+      elapsedTime / 1000
+    }seconds.`
+  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
+    // end of word
+    // Clear the typedValueElement for the new word
+    typedValueElement.value = ""
+    // move to the next word
+    wordIndex++
+    // reset the class name for all elements in quote
+    for (const wordElement of quoteElement.childNodes) {
+      wordElement.className = ""
+    }
+  }
+})
