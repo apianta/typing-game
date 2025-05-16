e92d8aed43621215fb0453bc8136ab0fbabd03a9
apianta <a.pianta92@gmail.com>
2025-05-16 09:54:03 -0400

    feat: add a few new quotes

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const quotes = [
   'What one man can invent another can discover.',
   'Nothing clears up a case so much as stating it to another person.',
   'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+  'A book must be the axe for the frozen sea within us.',
+  'To study the self is to forget the self.',
+  'Until you make the unconscious conscious, it will direct your life and you will call it fate.',
+  'I must not fear. Fear is the mind-killer.',
 ];
 // store the list of words and the index of the word the player is currently typing
 let words = [];
